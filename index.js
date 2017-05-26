ba74c8e9809eec887efefdc92ee278c9e9f41bba
kiddhustle <mail@frontendwebdeveloper.london>
2017-05-26 17:01:54 +0100

    pre render component examples

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const PROJECT_DIR = path.resolve(__dirname)
+const pug = require('pug')
+const fs = require('fs')
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -12,7 +14,40 @@ app.set('views', 'backend/views')
 app.set('view engine', 'pug')
 
 app.get('/', function (request, response) {
-  response.render('pages/index')
+  const partialsPath = `${PROJECT_DIR}/backend/views/partials`
+  const templatePaths = {
+    headers: {
+      h1: `${partialsPath}/h1.pug`,
+      h2: `${partialsPath}/h2.pug`,
+      h3: `${partialsPath}/h3.pug`,
+      h4: `${partialsPath}/h4.pug`
+    },
+    paragraphs: {
+      'p': `${partialsPath}/p.pug`,
+      'p_large': `${partialsPath}/p--large.pug`,
+      'p--small': `${partialsPath}/p--small.pug`,
+      'p--xsmall': `${partialsPath}/p--xsmall.pug`
+    },
+    links: {
+      'a': `${partialsPath}/a.pug`
+    }
+  }
+  // pre-render examples
+  const renderComponentExample = function (sectionName, componentName) {
+    let templatePath = templatePaths[sectionName][componentName]
+    if (!this[sectionName]) {
+      this[sectionName] = {}
+    }
+    this[sectionName][componentName] = pug.render(fs.readFileSync(templatePath, 'utf8'),
+      {filename: templatePath})
+  }
+  let output = {}
+  // go through template paths sections and populate output
+  Object.getOwnPropertyNames(templatePaths).forEach(section => {
+    Object.getOwnPropertyNames(templatePaths[section]).forEach(renderComponentExample.bind(output, section))
+  })
+
+  response.render('pages/index', output)
 })
 
 app.listen(app.get('port'), function () {
