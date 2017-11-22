c03ff8ba515ce2e8a4d7287458b4562b0a0f5028
eadpearce <e.pearce@tutanota.com>
2017-11-22 15:12:27 +0000

    Updated exopps script to copy over css, images and js

--- scripts/copy_header_footer_erb.rb
+++ scripts/copy_header_footer_erb.rb
@@ -1,16 +1,22 @@
 #!/usr/bin/ruby
 # encoding: UTF-8
 
-# this script copies the header/footer html files from great-pattern-library
+# this script copies all the header/footer files from great-pattern-library
 # and adds all the necessary variables and template logic
 # for erb templates used in export-opportunities
 
 # export-opportunities directory must exist parallel to great-pattern-library
 
+require 'fileutils'
+
 PATTLIB_DIR = "./shared-header-footer/"
-DIR_HEADER_FOOTER = "../export-opportunities/app/views/layouts/"
+DIR_HTML = "../export-opportunities/app/views/layouts/"
+DIR_IMG = "../export-opportunities/app/assets/images/logos/"
+DIR_CSS = "../export-opportunities/app/assets/stylesheets/"
+DIR_JS = "../export-opportunities/app/assets/javascripts/"
 
 def build_header()
+  puts "*** Building HEADER TEMPLATE ***"
   header = File.read(PATTLIB_DIR+"header.html")
   header = header.split(/(t\-links\"\>)(\s)/)
   header[2] = "\n        <% if not @current_user %>\n"
@@ -31,16 +37,17 @@ def build_header()
 end
 
 def build_footer()
+  puts "*** Building FOOTER TEMPLATE ***"
   footer = File.read(PATTLIB_DIR+"footer.html")
   footer = replace(footer)
   write_file('footer', footer)
 end
 
 def write_file(filename, data)
-  file = File.new("#{DIR_HEADER_FOOTER}_dit_#{filename}.html.erb", "r+")
+  file = File.new("#{DIR_HTML}_dit_#{filename}.html.erb", "r+")
   if file
     file.syswrite(data)
-    puts "Written #{DIR_HEADER_FOOTER}_dit_#{filename}.html.erb"
+    puts "Written #{DIR_HTML}_dit_#{filename}.html.erb"
   else
     puts "Unable to write to file"
   end
@@ -62,5 +69,35 @@ def replace(html)
   return html
 end
 
+def copy_images()
+  puts "*** Copying IMAGES ***"
+  src = Dir.glob("#{PATTLIB_DIR}*.{svg,png,gif}")
+  src.each do |file|
+    filename = File.basename file
+    FileUtils.cp(file, DIR_IMG+filename)
+    puts "Copied #{filename} to #{DIR_IMG+filename}"
+  end
+end
+
+def copy_css()
+  puts "*** Copying CSS ***"
+  file = "#{PATTLIB_DIR}style.css"
+  filename = File.basename file
+  FileUtils.cp(file, "#{DIR_CSS}header-footer.css")
+  puts "Copied #{filename} to #{DIR_CSS}header-footer.css"
+end
+
+def copy_js()
+  puts "*** Copying JS ***"
+  script = "#{PATTLIB_DIR}script.js"
+  FileUtils.cp(script, "#{DIR_JS}header-footer.js")
+  puts "Copied #{script} to #{DIR_JS}header-footer.js"
+  third_party = "#{PATTLIB_DIR}third-party.js"
+  FileUtils.cp(third_party, "#{DIR_JS}jquery3.2.0.js")
+end
+
 build_footer()
 build_header()
+copy_images()
+copy_css()
+copy_js()
