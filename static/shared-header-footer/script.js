e8b4fdd33eb5b9764d9b008da32d2625c3ff1e26
eadpearce <e.pearce@tutanota.com>
2017-11-23 15:01:17 +0000

    Removed SelectTracker JS - not needed

--- static/shared-header-footer/script.js
+++ static/shared-header-footer/script.js
@@ -1048,50 +1048,6 @@ dit.components.menu = (new function() {
 })(jQuery, dit.utils, dit.classes);
 
 
-/* Class: Select Tracker
- * ---------------------
- * Adds a label element to mirror the matched selected option
- * text of a <select> input, for enhanced display purpose.
- *
- * REQUIRES:
- * jquery
- * dit.js
- * dit.classes.js
- *
- **/
-(function($, classes) {
-
-  /* Constructor
-   * @$select (jQuery node) Target input element
-   **/
-  classes.SelectTracker = SelectTracker;
-  function SelectTracker($select) {
-    var SELECT_TRACKER = this;
-    var button, code, lang;
-
-    if(arguments.length && $select.length) {
-      this.$node = $(document.createElement("p"));
-      this.$node.attr("aria-hidden", "true");
-      this.$node.addClass("SelectTracker");
-      this.$select = $select;
-      this.$select.addClass("SelectTracker-Select");
-      this.$select.after(this.$node);
-      this.$select.on("change.SelectTracker", function() {
-        SELECT_TRACKER.update();
-      });
-
-      // Initial value
-      this.update();
-    }
-  }
-  SelectTracker.prototype = {};
-  SelectTracker.prototype.update = function() {
-    this.$node.text(this.$select.find(":selected").text());
-  }
-
-})(jQuery, dit.classes);
-
-
 // Language Selector Component Functionality.
 //
 // Requires...
